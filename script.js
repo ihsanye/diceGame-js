ba968c5b081fbbe75d197f96774795dc12cd5973
ihsanye <ihsany98@gmail.com>
2024-04-25 01:10:57 +0300

    straights and endgame

--- script.js
+++ script.js
@@ -101,6 +101,25 @@ const detectFullHouse = (arr) => {
     updateRadioOption(5, 0);
 };
 
+const checkForStraights = (arr) => {
+    const sortedNumbersArr = arr.sort((a, b) => a - b);
+    const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+    const uniqueNumbersStr = uniqueNumbersArr.join("");
+
+    const smallStraightsArr = ["1234", "2345", "3456"];
+    const largeStraightsArr = ["12345", "23456"];
+
+    if (smallStraightsArr.some(straight => uniqueNumbersStr.includes(straight))) {
+        updateRadioOption(3, 30);
+    }
+
+    if (largeStraightsArr.includes(uniqueNumbersStr)) {
+        updateRadioOption(4, 40);
+    }
+
+    updateRadioOption(5, 0);
+};
+
 const resetRadioOption = () => {
     scoreInputs.forEach((input) => {
         input.disabled = true;
@@ -142,6 +161,7 @@ rollDiceBtn.addEventListener("click", () => {
         updateStats();
         getHighestDuplicates(diceValuesArr);
         detectFullHouse(diceValuesArr);
+        rollDiceBtn(checkForStraights(diceValuesArr));
     }
 });
 
